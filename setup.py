d12fc9b179144d78d7bdbad7591023dc506cefa2
Junya Hayashi <junya.hayashi@groove-x.com>
2017-08-20 00:35:12 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name="todolist",
+    packages=find_packages(exclude=["test"]),
+    install_requires=[],
+    license="MIT",
+)
